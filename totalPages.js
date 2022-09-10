e69d5c4962c2ca87ce9697b52d901a82c8d6ba7c
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-10 21:48:58 +0700

    output7

--- totalPages.js
+++ totalPages.js
@@ -4,6 +4,9 @@ function totalPages(arrayItems, rowsPerPage) {
   if (!rowsPerPage) {
     return 1
   }
+  if (!arrayItems) {
+    return undefined
+  }
   return Math.ceil(arrayItems.length/rowsPerPage) 
 }
 module.exports = totalPages
